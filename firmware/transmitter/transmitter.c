7f63a6d370248416786111d854e7b7d0f7759572
Roman <roman.weintraub@gmail.com>
2024-11-11 01:48:46 +0100

    replace zephyr with freeRTOS and a blinker example

---
+++ firmware/transmitter/transmitter.c
@@ -0,0 +1,44 @@
+/*
+ * LED blink with FreeRTOS
+ */
+
+#include <FreeRTOS.h>
+#include <task.h>
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+
+struct led_task_arg {
+    int gpio;
+    int delay;
+};
+
+void led_task(void *p)
+{
+    struct led_task_arg *a = (struct led_task_arg *)p;
+
+    while (true) {
+        cyw43_arch_gpio_put(a->gpio, 1);
+        vTaskDelay(pdMS_TO_TICKS(a->delay));
+        cyw43_arch_gpio_put(a->gpio, 0);
+        vTaskDelay(pdMS_TO_TICKS(a->delay));
+    }
+}
+
+int main()
+{
+    stdio_init_all();
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed");
+        return -1;
+    }
+    printf("Start LED blink\n");
+
+    struct led_task_arg arg1 = { CYW43_WL_GPIO_LED_PIN, 1000 };
+    xTaskCreate(led_task, "LED_Task 1", 256, &arg1, 1, NULL);
+
+    vTaskStartScheduler();
+
+    while (true)
+        ;
+}
